cfd6f501ca3c40ea6a763019b6189547fb49b707
Noel Åsegg <noas0116@storsjogymnasiet.se>
2023-10-19 11:01:22 +0200

    gissa hemsida

---
+++ gissa/gissa.js
@@ -0,0 +1,49 @@
+const slumptal = Math.floor(Math.random() * 9) + 1
+let antalgissningar = 0
+
+
+slump = Math.random()
+
+function gissaTal() {
+    console.log(slump = (slumptal))
+    let textfelt_gissa = document.getElementById("gissa").value
+    let gissa_int = Number(textfelt_gissa)
+    let vinn = false
+    let p_svar = document.getElementById("p_svar")
+
+    if (antalgissningar < 3) {
+
+        if (slumptal === gissa_int) {
+            console.log("Rätt Svar")
+            vinn = true
+            p_svar.innerHTML = "DU VANN!!!!"
+
+
+        }
+        else {
+            console.log("fel svar!!!!")
+            if (antalgissningar === 2) {
+                p_svar.innerHTML = "Loserr!!!!"
+
+            }
+            else {
+                p_svar.innerHTML = `Fel Svar! ${gissa_int}`
+
+                // tömmer text fältet så använder sliupper sudda
+                document.getElementById("gissa").value = ""
+                document.getElementById("gissa").focus()
+            }
+        }
+
+        antalgissningar++
+
+    }
+    else {
+
+
+        p_svar.innerHTML = "Loser"
+    }
+
+}
+
+
